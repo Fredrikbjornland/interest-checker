7141cae058a88668df1b0d1a39a0717a59d7a605
Fredrik Bjørnland <bjornlandfredrik@gmail.com>
2023-01-29 14:12:53 +0100

    init

---
+++ views/Landing.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Text, View } from "react-native"
+
+export default function Landing() {
+    return (
+        <View className="h-full p-12 bg-slate-50">
+            <View className="flex flex-col">
+                <Text className="text-2xl">Kyte</Text>
+            </View>
+        </View>
+    )
+}
